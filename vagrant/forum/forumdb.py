fc817659dff3a1139d75a1deae18c6323bfb4ce3
Ryan Chan <ryanchanwo@gmail.com>
2017-09-06 23:05:48 +0100

    Complete "Python DB-API" forum backend (Part 3)
    Replace previous forum backend (in-memory Python list) to use the
    PostgreSQL "forum" database.

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,30 @@
 # "Database code" for the DB Forum.
-
 import datetime
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+import bleach
+import psycopg2
+
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  database = psycopg2.connect("dbname=forum")
+  cursor = database.cursor()
+
+  cursor.execute("SELECT content, time FROM posts ORDER BY time DESC;")
+
+  posts = cursor.fetchall()
+  database.close()
+  return [(bleach.clean(content), time, ) for content, time in posts]
+
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  database = psycopg2.connect("dbname=forum")
+  cursor = database.cursor()
 
+  time = datetime.datetime.now()
+  cursor.execute("INSERT INTO posts (content) VALUES (%(content)s);",
+                 {"content": bleach.clean(content), })
 
+  database.commit()
+  database.close()
